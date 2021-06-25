41da533b42dc62302f337d9d8a0880ace84e294d
SydNs <sydneynsiimbe@gmail.com>
2021-06-25 08:51:04 -0400

    made an adapter for walkthrough

---
+++ app/src/main/java/com/example/danmech/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.danmech.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.danmech.R
+
+class ViewPagerAdapter(
+    private val imagesList: List<Int>
+) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_viewpager, parent, false)
+        return ViewPagerHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        val image = imagesList[position]
+        holder.bind(image)
+    }
+
+    override fun getItemCount(): Int {
+        return imagesList.size
+    }
+
+
+    class ViewPagerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val ivSliderImage = itemView.findViewById<ImageView>(R.id.ivSliderImage)
+
+        fun bind(image: Int) {
+            ivSliderImage.setImageResource(image)
+        }
+    }
+}
