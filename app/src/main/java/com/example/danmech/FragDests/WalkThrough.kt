783fc21a358f31de5f0afdaeb72fc1bdfab1a6a6
SydNs <sydneynsiimbe@gmail.com>
2021-06-25 08:38:38 -0400

    making views for the walkthrough

--- app/src/main/java/com/example/danmech/FragDests/WalkThrough.kt
+++ app/src/main/java/com/example/danmech/FragDests/WalkThrough.kt
@@ -1,21 +1,13 @@
-package com.example.danmech
+package com.example.danmech.FragDests
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.danmech.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WalkThrough.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WalkThrough : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -23,15 +15,16 @@ class WalkThrough : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
+
+
         return inflater.inflate(R.layout.fragment_walk_through, container, false)
     }
 
@@ -48,10 +41,8 @@ class WalkThrough : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
                 WalkThrough().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+
+
                 }
     }
 }
