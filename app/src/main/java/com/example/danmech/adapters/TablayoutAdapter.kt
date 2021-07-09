9b47cd5b8ef22415010b1487cfed4af22662afe6
SydNS <sydneynsiimbe@gmail.com>
2021-07-09 13:26:27 +0300

    created the  adapter for the tablayout for authing

---
+++ app/src/main/java/com/example/danmech/adapters/TablayoutAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.danmech.adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.danmech.FragDests.Login
+import com.example.danmech.FragDests.SignUp
+
+class TablayoutAdapter(context: Context, fm: FragmentManager?, totalTabs: Int) :
+    FragmentPagerAdapter(fm!!) {
+    private val myContext: Context = context
+    var totalTabs: Int = totalTabs
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                Login()
+            }
+            1 -> {
+                SignUp()
+            }
+
+            else -> Login()
+        }
+    }
+
+    // this counts total number of tabs
+    override fun getCount(): Int {
+        return totalTabs
+    }
+
+}
