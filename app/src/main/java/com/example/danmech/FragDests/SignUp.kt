4c3ea66d47caa2942a9fd597a6430a9c5cb32ab2
SydNS <sydneynsiimbe@gmail.com>
2021-07-09 14:45:44 +0300

    imported the R class

--- app/src/main/java/com/example/danmech/FragDests/SignUp.kt
+++ app/src/main/java/com/example/danmech/FragDests/SignUp.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.danmech.R
 
 class SignUp : Fragment() {
     override fun onCreateView(
