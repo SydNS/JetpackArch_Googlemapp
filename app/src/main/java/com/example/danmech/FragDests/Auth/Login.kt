080189f63835fa5bc2fdc6af482bde34bd35e7b5
SydNS <sydneynsiimbe@gmail.com>
2021-07-10 17:40:53 +0300

    method for the navigation

--- app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
+++ app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
@@ -1,13 +1,16 @@
-package com.example.danmech.FragDests
+package com.example.danmech.FragDests.Auth
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.danmech.R
 
 class Login : Fragment() {
+    lateinit var loginbtn:Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,7 +19,17 @@ class Login : Fragment() {
     ): View? {
         activity?.theme?.applyStyle(R.style.loginstylemain,true)
         val v : View=inflater.inflate(R.layout.login,container,false)
+        loginbtn=v.findViewById(R.id.loginbtn)
+        login(v)
         return v
+    }
+
+    private fun login(v: View) {
+        v.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_authFragment_to_home_map)
+        }
+
     }
 
 }
