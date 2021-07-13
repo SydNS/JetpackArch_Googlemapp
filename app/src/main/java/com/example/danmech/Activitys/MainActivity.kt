d9d728ea34a5062af304cb16e56ab752ef6f40bd
SydNs <sydneynsiimbe@gmail.com>
2021-07-13 05:24:59 +0000

    using sharedpref classes to the help decide who is old and who is not

--- app/src/main/java/com/example/danmech/Activitys/MainActivity.kt
+++ app/src/main/java/com/example/danmech/Activitys/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.danmech.Activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.danmech.R
+import com.example.danmech.Sharedprefs.Moyosharedprefs
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var moyosharedprefs:Moyosharedprefs
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!moyosharedprefs.isUserNotOld  ){
+
+
+
+        }
 
 
     }
