5e22697565b1f02b805d208f56c37a3218e606ea
SydNs <sydneynsiimbe@gmail.com>
2021-07-13 05:09:18 +0000

    using shared preferences to tell who an old user is and who isnot

---
+++ app/src/main/java/com/example/danmech/Sharedprefs/Moyosharedprefs.kt
@@ -0,0 +1,53 @@
+package com.example.danmech.Sharedprefs
+
+import android.content.Context
+
+import android.content.SharedPreferences
+
+/**
+ * Class for Shared Preference
+ */
+class Moyosharedprefs (var context: Context) {
+    fun NotNew() {
+        val sharedPreferences =
+            context.getSharedPreferences("NotTheFirstTime", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("Old", "true")
+        editor.apply()
+    }
+
+    val isUserOld: Boolean
+        get() {
+            val sharedPreferences =
+                context.getSharedPreferences("NotTheFirstTime", Context.MODE_PRIVATE)
+            val isEmailEmpty = sharedPreferences.getString("Old", "")!!.isEmpty()
+            return isEmailEmpty
+        }
+
+
+    fun saveLoginDetails(email: String?, password: String?) {
+        val sharedPreferences =
+            context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("Email", email)
+        editor.putString("Password", password)
+        editor.apply()
+    }
+
+    val email: String?
+        get() {
+            val sharedPreferences =
+                context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE)
+            return sharedPreferences.getString("Email", "")
+        }
+
+    val isUserLogedOut: Boolean
+        get() {
+            val sharedPreferences =
+                context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE)
+            val isEmailEmpty = sharedPreferences.getString("Email", "")!!.isEmpty()
+            val isPasswordEmpty = sharedPreferences.getString("Password", "")!!.isEmpty()
+            return isEmailEmpty || isPasswordEmpty
+        }
+
+}
