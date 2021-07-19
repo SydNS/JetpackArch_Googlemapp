a5a4b6f0198464376e153e7eadb3faeb19ffa7a7
SydNs <sydneynsiimbe@gmail.com>
2021-07-19 13:38:01 +0000

    checking if there is a user currently

--- app/src/main/java/com/example/danmech/FragDests/Auth/SignUp.kt
+++ app/src/main/java/com/example/danmech/FragDests/Auth/SignUp.kt
@@ -4,6 +4,7 @@ package com.example.danmech.FragDests.Auth
 
 import android.app.ProgressDialog
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
@@ -15,6 +16,7 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.danmech.R
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.textfield.TextInputLayout
@@ -54,113 +56,124 @@ class SignUp : Fragment() {
         super.onCreate(savedInstanceState)
 
 // Initialize Firebase Auth
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
+        firebaseAuthListner = FirebaseAuth.AuthStateListener {
+            currentUser = FirebaseAuth.getInstance().currentUser!!
 
+            if (currentUser != null) {
+                NavHostFragment.findNavController(this)
+                    .navigate(R.id.action_authFragment_to_home_map)
+
+
+            }
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
+}
 
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
+override fun onStart() {
+    super.onStart()
+
+    // Check if user is signed in (non-null) and update UI accordingly.
+    val currentUser = auth.currentUser
+    if (currentUser != null) {
 //            reload();
-        }
     }
+}
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val v: View = inflater.inflate(R.layout.signup, container, false)
+override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    val v: View = inflater.inflate(R.layout.signup, container, false)
 
 
-        private var loadingBar: ProgressDialog? = null
+    private var loadingBar: ProgressDialog? = null
 //        hooking views to e used in the class
-        username = v.findViewById(R.id.username)
-        useremailaddress = v.findViewById(R.id.emailaddress)
-        userpassword = v.findViewById(R.id.password)
-        userpassword2 = v.findViewById(R.id.password2)
+    username = v.findViewById(R.id.username)
+    useremailaddress = v.findViewById(R.id.emailaddress)
+    userpassword = v.findViewById(R.id.password)
+    userpassword2 = v.findViewById(R.id.password2)
 
 
 
-        signupbtn = v.findViewById(R.id.signupbtn)
-        signupbtn.setOnClickListener {
+    signupbtn = v.findViewById(R.id.signupbtn)
+    signupbtn.setOnClickListener {
 
-            val email: String = useremailaddress.editText?.text.toString().trim()
-            val password: String = userpassword.editText?.text.toString().trim()
-            val password2: String = userpassword2.editText?.text.toString().trim()
+        val email: String = useremailaddress.editText?.text.toString().trim()
+        val password: String = userpassword.editText?.text.toString().trim()
+        val password2: String = userpassword2.editText?.text.toString().trim()
 
-            if (TextUtils.isEmpty(email)) {
-                Toast.makeText(
-                    requireActivity(),
-                    "Enter email address!",
-                    Toast.LENGTH_SHORT
-                ).show()
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(
+                requireActivity(),
+                "Enter email address!",
+                Toast.LENGTH_SHORT
+            ).show()
 
-            }
-
-            if (TextUtils.isEmpty(password)) {
-                userpassword.error = "Password is empty"
-                Toast.makeText(
-                    requireActivity(),
-                    "Enter password!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            if (password.length < 6) {
-                userpassword.error = "Password is short"
-                Toast.makeText(
-                    requireActivity(),
-                    "Password too short, enter minimum 6 characters!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
-                        val user = auth.currentUser
-//                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            requireActivity(), "Authentication failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-//                        updateUI(null)
-                    }
-                }
-
-
-
-
-            layoutwithtabs = activity?.findViewById(R.id.fragment_auth)!!
-
-            tabs = layoutwithtabs.findViewById(R.id.tabs)
-            tabs.getTabAt(0)
         }
 
+        if (TextUtils.isEmpty(password)) {
+            userpassword.error = "Password is empty"
+            Toast.makeText(
+                requireActivity(),
+                "Enter password!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        if (password.length < 6) {
+            userpassword.error = "Password is short"
+            Toast.makeText(
+                requireActivity(),
+                "Password too short, enter minimum 6 characters!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+//                        updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        requireActivity(), "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+//                        updateUI(null)
+                }
+            }
 
 
 
 
+        layoutwithtabs = activity?.findViewById(R.id.fragment_auth)!!
 
-
-        return v
+        tabs = layoutwithtabs.findViewById(R.id.tabs)
+        tabs.getTabAt(0)
     }
 
 
-    override fun onStop() {
-        super.onStop()
-        firebaseAuthListner?.let { auth?.removeAuthStateListener(it) }
-    }
+
+
+
+
+
+    return v
+}
+
+
+override fun onStop() {
+    super.onStop()
+    firebaseAuthListner?.let { auth?.removeAuthStateListener(it) }
+}
 
 
 }
