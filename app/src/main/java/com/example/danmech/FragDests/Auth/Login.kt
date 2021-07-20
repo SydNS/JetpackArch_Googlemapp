d503606887e5ce3d73016bca8e615ec9d760b670
SydNs <sydneynsiimbe@gmail.com>
2021-07-21 00:51:48 +0300

    capture user inputs and doing authenticating withthe firebase on them

--- app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
+++ app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
@@ -3,12 +3,15 @@
 package com.example.danmech.FragDests.Auth
 
 import android.app.ProgressDialog
+import android.graphics.Color
+import android.graphics.Color.red
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
@@ -21,7 +24,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 class Login : Fragment() {
     lateinit var loginbtn: Button
-    lateinit var logout_customer_btn: Button
+    lateinit var loginbanner: TextView
     lateinit var login_emailaddress: TextInputLayout
     lateinit var login_password: TextInputLayout
 
@@ -65,42 +68,61 @@ class Login : Fragment() {
 
         val v: View = inflater.inflate(R.layout.login, container, false)
         loginbtn = v.findViewById(R.id.loginbtn)
+        loginbanner = v.findViewById(R.id.loginbanner)
         login_emailaddress = v.findViewById(R.id.login_emailaddress)
         login_password = v.findViewById(R.id.login_password)
 
-        login(loginbtn)
+        login(loginbtn,login_emailaddress,login_password,loginbanner)
 
 
         return v
     }
 
-    private fun login(v: Button) {
+    private fun login(v: Button,em:TextInputLayout,pswd:TextInputLayout,loginbanner:TextView) {
         v.setOnClickListener {
             val uemail: String = login_emailaddress.editText?.text.toString().trim()
             val upassd: String = login_password.editText?.text.toString().trim()
 
             //          checking if the value from the email field is empty or  not
             if (TextUtils.isEmpty(uemail)) {
-                Toast.makeText(
+                em.isErrorEnabled=true
+                em.error="Enter Email Address"
+                /*Toast.makeText(
                     requireActivity(),
                     "Enter email address!",
                     Toast.LENGTH_SHORT
-                ).show()
+                ).show()*/
 
             }
             if (TextUtils.isEmpty(upassd)) {
-                Toast.makeText(
-                    requireActivity(),
-                    "Enter password!",
-                    Toast.LENGTH_SHORT
-                ).show()
+
+                em.isErrorEnabled=true
+                em.error="Enter Password"
+//                Toast.makeText(
+//                    requireActivity(),
+//                    "Enter password!",
+//                    Toast.LENGTH_SHORT
+//                ).show()
 
             }
+            loginusers(uemail,upassd,em,pswd,loginbanner)
 
-            NavHostFragment.findNavController(this)
-                .navigate(R.id.action_authFragment_to_home_map)
         }
 
+    }
+
+    private fun loginusers(uemail: String, upassd: String,em:TextInputLayout,pswd: TextInputLayout,loginbanner:TextView) {
+        auth.signInWithEmailAndPassword(uemail,upassd).addOnCompleteListener {
+            if (it.isSuccessful){
+                NavHostFragment.findNavController(this)
+                    .navigate(R.id.action_authFragment_to_home_map)
+            }else{
+                loginbanner.text= it.exception?.message.toString()
+                Color.parseColor("#FF001E")
+
+
+            }
+        }
     }
 
 
