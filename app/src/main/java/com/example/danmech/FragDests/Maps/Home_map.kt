85e6807b342fbd446b3a8e20f42022278604a870
SydNs <sydneynsiimbe@gmail.com>
2021-07-20 01:21:56 +0000

    implementing some google maps in the app

--- app/src/main/java/com/example/danmech/FragDests/Maps/Home_map.kt
+++ app/src/main/java/com/example/danmech/FragDests/Maps/Home_map.kt
@@ -1,6 +1,9 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.danmech.FragDests.Maps
 
 import android.content.Context
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -11,17 +14,25 @@ import android.widget.Button
 import androidx.navigation.fragment.NavHostFragment
 import com.example.danmech.R
 import com.example.danmech.Sharedprefs.Moyosharedprefs
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class Home_map : Fragment() {
+class Home_map : Fragment(), OnMapReadyCallback,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+    LocationListener {
     lateinit var logout_customer_btn: Button
-
     private lateinit var moyosharedprefs: Moyosharedprefs
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,6 +83,54 @@ class Home_map : Fragment() {
 
         return v }
 
+    private val closestDeliiverer: Unit
+        private get() {
+            val geoFire: GeoFire = GeoFire(DriverAvailableRef)
+            geoQuery = geoFire.queryAtLocation(
+                GeoLocation(
+                    CustomerPickUpLocation!!.latitude,
+                    CustomerPickUpLocation!!.longitude
+                ), radius.toDouble()
+            )
+            geoQuery?.removeAllListeners()
+            geoQuery?.addGeoQueryEventListener(object : GeoQueryEventListener {
+                public override fun onKeyEntered(key: String, location: GeoLocation) {
+                    //anytime the driver is called this method will be called
+                    //key=driverID and the location
+                    if (!driverFound!! && requestType) {
+                        driverFound = true
+                        driverFoundID = key
+
+
+                        //we tell driver which customer he is going to have
+                        DriversRef = FirebaseDatabase.getInstance().getReference().child("Users")
+                            .child("Drivers").child(
+                                driverFoundID!!
+                            )
+                        val driversMap = HashMap<String?, String?>()
+                        driversMap["CustomerRideID"] = customerID
+                        DriversRef!!.updateChildren(driversMap as Map<String, Any>)
+
+                        //Show driver location on customerMapActivity
+                        GettingDriverLocation()
+                        CallCabCarButton!!.setText("Looking for Mechanic Location...")
+                    }
+                }
+
+                public override fun onKeyExited(key: String) {}
+                public override fun onKeyMoved(key: String, location: GeoLocation) {}
+                public override fun onGeoQueryReady() {
+                    if (!driverFound!!) {
+                        radius += 1
+                        closetDriverCab
+                    }
+                }
+
+                override fun onGeoQueryError(error: DatabaseError) {}
+            })
+        }
+
+
     fun isUserOld(): Boolean {
         val sharedPreferences =
             requireActivity().getSharedPreferences("", Context.MODE_PRIVATE)
@@ -91,5 +150,24 @@ class Home_map : Fragment() {
                 .navigate(R.id.action_home_map_to_authFragment)
         }
 
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+
+    public override fun onConnectionSuspended(i: Int) {}
+    public override fun onConnectionFailed(connectionResult: ConnectionResult) {}
+    public override fun onLocationChanged(location: Location) {
+        //getting the updated location
+        LastLocation = location
+        val latLng: LatLng = LatLng(location.latitude, location.longitude)
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(12f))
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("Not yet implemented")
     }
 }
