4f542e5fc7a34b8c1f2a33fdc4c72453a17fd84a
SydNs <sydneynsiimbe@gmail.com>
2021-07-20 03:29:03 +0000

    doing away with australian callbacks

--- app/src/main/java/com/example/danmech/FragDests/Maps/Home_map.kt
+++ app/src/main/java/com/example/danmech/FragDests/Maps/Home_map.kt
@@ -61,7 +61,7 @@ class Home_map : Fragment(), OnMapReadyCallback,
     private var Logout: Button? = null
     private var SettingsButton: Button? = null
     private var CallDelivererButton: Button? = null
-    private var callingbtn: ImageView? = null
+    private var callingbtn: Button? = null
     private var mAuth: FirebaseAuth? = null
     private var currentUser: FirebaseUser? = null
     private var CustomerDatabaseRef: DatabaseReference? = null
@@ -97,20 +97,20 @@ class Home_map : Fragment(), OnMapReadyCallback,
     }
 
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+//    private val callback = OnMapReadyCallback { googleMap ->
+//        /**
+//         * Manipulates the map once available.
+//         * This callback is triggered when the map is ready to be used.
+//         * This is where we can add markers or lines, add listeners or move the camera.
+//         * In this case, we just add a marker near Sydney, Australia.
+//         * If Google Play services is not installed on the device, the user will be prompted to
+//         * install it inside the SupportMapFragment. This method will only be triggered once the
+//         * user has installed Google Play services and returned to the app.
+//         */
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -305,11 +305,11 @@ class Home_map : Fragment(), OnMapReadyCallback,
             })
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+//        mapFragment?.getMapAsync(callback)
+//    }
 
     private fun logout(v: Button) {
         v.setOnClickListener {
