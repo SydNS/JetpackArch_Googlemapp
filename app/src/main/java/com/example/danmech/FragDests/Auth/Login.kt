8421ba1a8ca0626d73b732fe04669ef3474f2fd6
SydNs <sydneynsiimbe@gmail.com>
2021-07-26 21:56:03 +0300

    setting the error values to nil extracted stringss removed the unused vars

--- app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
+++ app/src/main/java/com/example/danmech/FragDests/Auth/Login.kt
@@ -4,15 +4,12 @@ package com.example.danmech.FragDests.Auth
 
 import android.app.ProgressDialog
 import android.graphics.Color
-import android.graphics.Color.red
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.example.danmech.R
@@ -28,6 +25,9 @@ class Login : Fragment() {
     lateinit var login_emailaddress: TextInputLayout
     lateinit var login_password: TextInputLayout
 
+    lateinit var uemail: String
+    lateinit var upassd: String
+
 
     //firebase variables
     private lateinit var auth: FirebaseAuth
@@ -37,7 +37,6 @@ class Login : Fragment() {
     private lateinit var loadingBar: ProgressDialog
     private lateinit var currentUser: FirebaseUser
     private lateinit var currentUserId: String
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,59 +64,77 @@ class Login : Fragment() {
 
         loadingBar = ProgressDialog(activity)
 
-
         val v: View = inflater.inflate(R.layout.login, container, false)
         loginbtn = v.findViewById(R.id.loginbtn)
         loginbanner = v.findViewById(R.id.loginbanner)
         login_emailaddress = v.findViewById(R.id.login_emailaddress)
         login_password = v.findViewById(R.id.login_password)
 
-        login(loginbtn,login_emailaddress,login_password,loginbanner)
+        login(loginbtn, login_emailaddress, login_password, loginbanner)
 
 
         return v
     }
 
-    private fun login(v: Button,em:TextInputLayout,pswd:TextInputLayout,loginbanner:TextView) {
+    private fun login(
+        v: Button,
+        em: TextInputLayout,
+        pswd: TextInputLayout,
+        loginbanner: TextView
+    ) {
         v.setOnClickListener {
-            val uemail: String = login_emailaddress.editText?.text.toString().trim()
-            val upassd: String = login_password.editText?.text.toString().trim()
 
-            //          checking if the value from the email field is empty or  not
-            if (TextUtils.isEmpty(uemail)) {
-                em.isErrorEnabled=true
-                em.error="Enter Email Address"
-                /*Toast.makeText(
-                    requireActivity(),
-                    "Enter email address!",
-                    Toast.LENGTH_SHORT
-                ).show()*/
+//            setting the errors flags to non
+            em.isErrorEnabled = false
+            em.error = ""
+            pswd . isErrorEnabled = false
+            pswd.error = ""
 
+            uemail = login_emailaddress.editText?.text.toString().trim()
+            upassd = login_password.editText?.text.toString().trim()
+
+            //checking if the value from the email field is empty or  not
+            if (uemail.isNotEmpty() or upassd.isNotEmpty()) when {
+                uemail.isNotEmpty() and upassd.isEmpty() -> {
+                    loginbanner.text = getString(R.string.loginfillinpassword)
+                    em.isErrorEnabled = true
+                    em.error = getString(R.string.loginenterpassword)
+                }
+                uemail.isNotEmpty() and upassd.isEmpty() -> {
+                    loginbanner.text = getString(R.string.loginfillinemail)
+                    pswd.isErrorEnabled = true
+                    pswd.error = getString(R.string.loginfillinemail2)
+
+                }
+                uemail.isNotEmpty() and upassd.isNotEmpty() -> {
+                    loginbanner.text = getString(R.string.loginthankyou)
+                    loginusers(uemail, upassd, loginbanner)
+                }
+                else -> {
+                    loginbanner.text = getString(R.string.loginallfields)
+                    em.isErrorEnabled = true
+                    em.error = getString(R.string.loginfillinemail2)
+                    pswd.isErrorEnabled = true
+                    pswd.error =getString(R.string.loginenterpassword)
+
+
+                }
             }
-            if (TextUtils.isEmpty(upassd)) {
-
-                em.isErrorEnabled=true
-                em.error="Enter Password"
-//                Toast.makeText(
-//                    requireActivity(),
-//                    "Enter password!",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-
-            }
-            loginusers(uemail,upassd,em,pswd,loginbanner)
 
         }
-
     }
 
-    private fun loginusers(uemail: String, upassd: String,em:TextInputLayout,pswd: TextInputLayout,loginbanner:TextView) {
-        auth.signInWithEmailAndPassword(uemail,upassd).addOnCompleteListener {
-            if (it.isSuccessful){
+    private fun loginusers(
+        uemail: String,
+        upassd: String,
+        loginbanner: TextView
+    ) {
+        auth.signInWithEmailAndPassword(uemail, upassd).addOnCompleteListener {
+            if (it.isSuccessful) {
                 NavHostFragment.findNavController(this)
                     .navigate(R.id.action_authFragment_to_home_map)
-            }else{
-                loginbanner.text= it.exception?.message.toString()
+            } else {
+                loginbanner.text = it.exception?.message.toString()
                 Color.parseColor("#FF001E")
 
 
