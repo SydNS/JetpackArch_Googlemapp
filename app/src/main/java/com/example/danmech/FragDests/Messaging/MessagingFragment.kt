897e47fb63fb79a4374b921a35abf836642708ce
SydNs <sydneynsiimbe@gmail.com>
2021-07-26 20:23:22 +0300

    this is to handle the communication btn the involved parties

--- app/src/main/java/com/example/danmech/FragDests/Messaging/MessagingFragment.kt
+++ app/src/main/java/com/example/danmech/FragDests/Messaging/MessagingFragment.kt
@@ -1,11 +1,17 @@
 package com.example.danmech.FragDests.Messaging
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
 import com.example.danmech.R
+import com.example.danmech.adapters.TablayoutAdapter
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.tabs.TabLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +28,14 @@ class MessagingFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var tabs: TabLayout
+    lateinit var appbarauth: AppBarLayout
+    lateinit var vp: ViewPager
+    lateinit var toolbar:androidx.appcompat.widget.Toolbar
+    lateinit var tablayoutAdapter: TablayoutAdapter
+    lateinit var supoortFragmentManager: FragmentManager
+    lateinit var ctx: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -29,6 +43,12 @@ class MessagingFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        ctx=context
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
