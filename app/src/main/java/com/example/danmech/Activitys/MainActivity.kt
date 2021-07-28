234d846fb0cfdf86c2ab28c05c32b1aa3c795568
SydNs <sydneynsiimbe@gmail.com>
2021-07-28 04:31:40 +0300

    requesting user to give permission to location services in the activity

--- app/src/main/java/com/example/danmech/Activitys/MainActivity.kt
+++ app/src/main/java/com/example/danmech/Activitys/MainActivity.kt
@@ -1,8 +1,15 @@
+@file:Suppress("DEPRECATED_IDENTITY_EQUALS")
+
 package com.example.danmech.Activitys
 
+import android.Manifest
 import android.R.attr.fragment
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.danmech.FragDests.Auth.AuthFragment
@@ -16,20 +23,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        moyosharedprefs= Moyosharedprefs(this)
-//        authFragment=AuthFragment()
-//
-//
-//        val manager: FragmentManager = supportFragmentManager
-//        val transaction: FragmentTransaction = manager.beginTransaction()
-//
-//        transaction.replace(R.id.container, authFragment)
-//        transaction.commit()
-
-//        if (!moyosharedprefs.isUserNotOld  ){
-//            authFragment.supoortFragmentManager.
-//
-//        }
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) !==
+            PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION) ===
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
 
 
     }
